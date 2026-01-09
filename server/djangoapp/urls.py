b79f0f7b721587b1b4fc10b442887d97719433cf
deepaksh-mca <deepakmca207@gmail.com>
2026-01-09 02:26:17 -0500

    code commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,7 +9,8 @@ from django.views.generic import TemplateView
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='register', view=views.registration, name='register'),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
