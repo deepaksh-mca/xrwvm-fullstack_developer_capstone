e393c7e97af8f47d0c1e41518998d274f62d9c80
deepaksh-mca <deepakmca207@gmail.com>
2026-01-14 08:17:41 -0500

    code commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from djangoapp.restapis import get_request
+from djangoapp.restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
