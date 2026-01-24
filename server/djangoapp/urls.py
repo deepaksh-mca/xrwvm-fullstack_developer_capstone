352d091665ac0cff9a01e02fe6f4fbd7705f504d
deepaksh-mca <deepakmca207@gmail.com>
2026-01-24 11:11:41 -0500

    code commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,8 +16,11 @@ urlpatterns = [
     # path for dealer reviews view
     # path for add a review view
     path('get_dealers', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', views.get_dealerships,
-          name='get_dealers_by_state'),
+    path(
+        route='get_dealers/<str:state>/',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
     path('dealer/<int:dealer_id>', views.get_dealer_details,
           name='dealer_details'),
     path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews,
